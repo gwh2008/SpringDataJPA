827b05d0835cf708dfeb824f8bce24ca50760743
gaowh <gwh_2014@163.com>
2024-01-10 10:35:09 +0800

    注释掉cxf

--- src/main/java/com/vcyber/user/client/CxfClient.java
+++ src/main/java/com/vcyber/user/client/CxfClient.java
@@ -1,58 +1,58 @@
-package com.vcyber.user.client;
-
-import org.apache.cxf.endpoint.Client;
-import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
-import org.apache.cxf.jaxws.endpoint.dynamic.JaxWsDynamicClientFactory;
-import com.vcyber.user.service.CommonService;
-
-public class CxfClient {
-    public static void main(String[] args) {
-        //cl1();
-        cl2();
-    }
-
-    /**
-     * 方式1.代理类工厂的方式,需要拿到对方的接口
-     */
-    public static void cl1() {
-        try {
-            // 接口地址
-            String address = "http://localhost:8080/services/CommonService?wsdl";
-            // 代理工厂
-            JaxWsProxyFactoryBean jaxWsProxyFactoryBean = new JaxWsProxyFactoryBean();
-            // 设置代理地址
-            jaxWsProxyFactoryBean.setAddress(address);
-            // 设置接口类型
-            jaxWsProxyFactoryBean.setServiceClass(CommonService.class);
-            // 创建一个代理接口实现
-            CommonService cs = (CommonService) jaxWsProxyFactoryBean.create();
-            // 数据准备
-            String userName = "柠萌LemmonTree";
-            // 调用代理接口的方法调用并返回结果
-            String result = cs.sayHello(userName);
-            System.out.println("返回结果:" + result);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * 动态调用方式
-     */
-    public static void cl2() {
-        // 创建动态客户端
-        JaxWsDynamicClientFactory dcf = JaxWsDynamicClientFactory.newInstance();
-        Client client = dcf.createClient("http://localhost:8080/services/CommonService?wsdl");
-        // 需要密码的情况需要加上用户名和密码
-        // client.getOutInterceptors().add(new ClientLoginInterceptor(USER_NAME,
-        // PASS_WORD));
-        Object[] objects = new Object[0];
-        try {
-            // invoke("方法名",参数1,参数2,参数3....);
-            objects = client.invoke("sayHello", "西瓜LemmonTree");
-            System.out.println("返回数据:" + objects[0]);
-        } catch (java.lang.Exception e) {
-            e.printStackTrace();
-        }
-    }
-}
+//package com.vcyber.user.client;
+//
+//import org.apache.cxf.endpoint.Client;
+//import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+//import org.apache.cxf.jaxws.endpoint.dynamic.JaxWsDynamicClientFactory;
+//import com.vcyber.user.service.CommonService;
+//
+//public class CxfClient {
+//    public static void main(String[] args) {
+//        //cl1();
+//        cl2();
+//    }
+//
+//    /**
+//     * 方式1.代理类工厂的方式,需要拿到对方的接口
+//     */
+//    public static void cl1() {
+//        try {
+//            // 接口地址
+//            String address = "http://localhost:8080/services/CommonService?wsdl";
+//            // 代理工厂
+//            JaxWsProxyFactoryBean jaxWsProxyFactoryBean = new JaxWsProxyFactoryBean();
+//            // 设置代理地址
+//            jaxWsProxyFactoryBean.setAddress(address);
+//            // 设置接口类型
+//            jaxWsProxyFactoryBean.setServiceClass(CommonService.class);
+//            // 创建一个代理接口实现
+//            CommonService cs = (CommonService) jaxWsProxyFactoryBean.create();
+//            // 数据准备
+//            String userName = "柠萌LemmonTree";
+//            // 调用代理接口的方法调用并返回结果
+//            String result = cs.sayHello(userName);
+//            System.out.println("返回结果:" + result);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    /**
+//     * 动态调用方式
+//     */
+//    public static void cl2() {
+//        // 创建动态客户端
+//        JaxWsDynamicClientFactory dcf = JaxWsDynamicClientFactory.newInstance();
+//        Client client = dcf.createClient("http://localhost:8080/services/CommonService?wsdl");
+//        // 需要密码的情况需要加上用户名和密码
+//        // client.getOutInterceptors().add(new ClientLoginInterceptor(USER_NAME,
+//        // PASS_WORD));
+//        Object[] objects = new Object[0];
+//        try {
+//            // invoke("方法名",参数1,参数2,参数3....);
+//            objects = client.invoke("sayHello", "西瓜LemmonTree");
+//            System.out.println("返回数据:" + objects[0]);
+//        } catch (java.lang.Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
--- src/main/java/com/vcyber/user/configuration/CxfConfig.java
+++ src/main/java/com/vcyber/user/configuration/CxfConfig.java
@@ -1,24 +1,24 @@
-package com.vcyber.user.configuration;
-
-import com.vcyber.user.service.CommonService;
-import org.apache.cxf.Bus;
-import org.apache.cxf.jaxws.EndpointImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import javax.xml.ws.Endpoint;
-
-@Configuration
-public class CxfConfig {
-    @Autowired(required = false)
-    private Bus bus;
-    @Autowired(required = false)
-    CommonService commonService;
-    @Bean
-    public Endpoint endpoint(){
-        EndpointImpl endpoint=new EndpointImpl(bus,commonService);
-        endpoint.publish("/CommonService");
-
-        return endpoint;
-    }
-}
+//package com.vcyber.user.configuration;
+//
+//import com.vcyber.user.service.CommonService;
+//import org.apache.cxf.Bus;
+//import org.apache.cxf.jaxws.EndpointImpl;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import javax.xml.ws.Endpoint;
+//
+//@Configuration
+//public class CxfConfig {
+//    @Autowired(required = false)
+//    private Bus bus;
+//    @Autowired(required = false)
+//    CommonService commonService;
+//    @Bean
+//    public Endpoint endpoint(){
+//        EndpointImpl endpoint=new EndpointImpl(bus,commonService);
+//        endpoint.publish("/CommonService");
+//
+//        return endpoint;
+//    }
+//}
